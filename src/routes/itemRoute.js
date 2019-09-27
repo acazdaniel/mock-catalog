5df6fc3d4b1250e695a309fd50c957e162292961
Daniel <daniel.acaz@becommerce.com.br>
2019-09-27 12:52:02 -0300

    Revert "modified to real adivertisements"
    This reverts commit 5244f505944e32535b9f88d38b1542f41b5e5a06.

--- src/routes/itemRoute.js
+++ src/routes/itemRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/itemController')
-router.get('/*/items/search', controller.get)
+router.get('/12345/items/search', controller.get)
 module.exports = router;
